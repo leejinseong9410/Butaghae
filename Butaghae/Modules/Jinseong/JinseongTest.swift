baba68e847cbc98f6af344449cf77ed4c233286f
leejinseong9410 <81233734+leejinseong9410@users.noreply.github.com>
2022-07-08 12:11:06 +0900

    jinseongTest

---
+++ Butaghae/Modules/Jinseong/JinseongTest.swift
@@ -0,0 +1,13 @@
+//
+//  JinseongTest.swift
+//  Butaghae
+//
+//  Created by MacBookPro on 2022/07/08.
+//
+
+import Foundation
+
+
+class JinseongTestViewController {
+    var jinseongTest = "JinseongTest"
+}
